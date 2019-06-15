13e12f48693961feb9f4175a19976e434f7ea703
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-15 20:46:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
-def reverse_each_word(array)
-  
-  old_array = string.split(" ")
-  new_array = []
-  old_array.each do |string|
-    new_array << string.reverse
+def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
   end
-  new_array.join(" ")
+  return_array.join(" ")
 end
-  
